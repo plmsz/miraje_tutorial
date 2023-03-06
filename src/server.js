9750f60cb4fa8625d8989a58d1abe17c4e70c8cb
Paloma <85586862+plmsz@users.noreply.github.com>
2023-03-06 16:35:59 -0300

    2 static post

--- src/server.js
+++ src/server.js
@@ -10,6 +10,12 @@ export default function (){
                     { id: 3, text: "Work out" },
                 ],
             }))
+            let newId = 4
+            this.post('api/reminders', (schema, request)=>{
+            let attrs = JSON.parse(request.requestBody)
+            attrs.id = newId++
+            return { reminder: attrs}
+            })
         }
     })
 }
