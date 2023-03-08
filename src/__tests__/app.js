dda0cb3ff659a30e35861d67e030012624edbc48
Paloma <85586862+plmsz@users.noreply.github.com>
2023-03-08 17:57:23 -0300

    8 testing

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -1,1 +1,19 @@
-// Welcome to the test file for the tutorial!
+import { visit } from "../lib/test-helpers"
+import { screen, waitForElementToBeRemoved } from "@testing-library/react"
+import makeServer from "../server"
+
+describe('Reminders', ()=> {
+    test("it shows a message when there are no reminders", async () => {
+      makeServer('test')
+      visit("/")
+      await waitForElementToBeRemoved(() => screen.getByText("Loading..."))
+    
+      expect(screen.getByText("All done!")).toBeInTheDocument()
+    })
+})
+
+// The All screen shows all Reminders, along with tags showing which List they belong to
+// A List only shows Reminders that belong to that List
+// The user can delete a Reminder
+// The user can create a List
+// The user can delete a List
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-import { Model, RestSerializer, belongsTo, createServer, hasMany } from "miragejs";
+import { Factory, Model, RestSerializer, belongsTo, createServer, hasMany } from "miragejs";
 
-export default function (){
+export default function (environment='development') {
     createServer({
+        environment,
         serializers: {
             reminder: RestSerializer.extend({
                 include: ['list'],
@@ -16,11 +17,24 @@ export default function (){
                 list: belongsTo()
             }),
         },
+        factories: {
+            list: Factory.extend({
+                name(i){
+                    return `List ${i}`  
+                }
+            }),
+            reminder: Factory.extend({
+                text(i){
+                    return `Reminder ${i}`
+                }
+                }),
+        },
         seeds(server){
+            server.create('list', {
+                reminders: server.createList('reminder', 2)
+            })
+            server.createList('reminder',3)
             server.create('reminder', {text: 'Walk the dog'})
-            server.create("reminder", { text: "Take out the trash" })
-            server.create("reminder", { text: "Work out" })
-
             let homeList = server.create("list", { name: "Home"})
             server.create("reminder", { list: homeList, text: "Do taxes" })
             let workList = server.create("list", { name: "Work"})
@@ -40,6 +54,16 @@ export default function (){
             })
             this.get('api/lists', (schema)=>{
                 return schema.lists.all()
+            })
+            this.post('/api/lists', (schema, request)=>{
+                let attrs = JSON.parse(request.requestBody)
+                return schema.lists.create(attrs)
+            })
+            this.delete('api/lists/:id', (schema, request)=>{
+                let id = request.params.id;
+                let list = schema.lists.find(id);
+                list.reminders.destroy();
+                return list.destroy();
             })
             this.get('api/lists/:id/reminders', (schema,request)=>{
                 let listId = request.params.id
